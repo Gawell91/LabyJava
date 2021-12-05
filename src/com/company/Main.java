858f579519e4f381328b17115c168f21d2fb0f33
Budow <gawel91@onet.pl>
2021-12-05 19:44:55 +0100

    Zadania od 1 do 6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,8 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
-
+package com.company;
 
 public class Main {
-    public Main() {
-    }
 
     public static void main(String[] args) {
-        new Animal("Burek", true, 3, true, 5.0D);
-        new Animal("Marek", false, 7, false, 2.5D);
-        new Car("Porshe", "911", 2004, "Black", 3.0D);
-        Phone firstPhone = new Phone("Apple", "iPhone 11", 2020, 8, 64);
-        System.out.println(firstPhone);
+	// write your code here
     }
 }
